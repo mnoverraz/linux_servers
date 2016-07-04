0cfd75d5f21676cc6ee355b81233b19d5bb59159
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2016-07-04 17:23:39 +0200

    Add virtualbox GestAddition

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 # Update du system
 apt-get update && apt-get upgrade -y
+
+# VirtualBox GestAddition
+apt-get install -y build-essential module-assistant
+m-a -i prepare
+sh /media/cdrom/VBoxLinuxAdditions.run
