267e0f81f084db3eea759b584b1dc8be82f122a9
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2016-07-04 17:26:45 +0200

    Add aliases to the bash

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ sh /media/cdrom/VBoxLinuxAdditions.run
 # Installation des logiciels de bases
 apt-get install -y sudo ssh git curl
 adduser mnoverraz sudo
+
+# Création des aliases et personnalisation du bash
+echo "# Personnalisations apportées par Mathieu Noverraz" >> /home/mnoverraz/.bashrc
+echo "alias ls='ls -la --color=auto'" >> /home/mnoverraz/.bashrc
